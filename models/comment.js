7a41f78978ca6dbbbc6ccea5f24b3455736a30cf
jeongmin <jeongjeong93@gmail.com>
2022-04-11 13:04:05 +0900

    댓글 커밋

---
+++ models/comment.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const commentsSchema = new mongoose.Schema({
+    post_id: {
+        type: Number,
+        require: true,
+        // unique: true,
+    },
+    comment_id: {
+        type: Number,
+        required: true,
+        // unique: true,
+    },
+    // postId: {
+    //     type: Number,
+    //     required: true,
+    //     unique: true,
+    // },
+    // commentId: {
+    //     type: Number,
+    //     required: true,
+    //     unique: true,
+    // },
+    user_name: {
+        type: String,
+        require: true,
+    },
+    // title: {
+    //     type: String,
+    //     required: true,
+    // },
+    // content: {
+    //     type: String,
+    //     required: true,
+    // },
+    comment: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Number,
+        required: true,
+    },
+    // createdAt: {
+    //     type: Date,
+    //     default:  Date.now(),
+    //     required: true,
+    // },
+},
+    // { timestamps: true } // createdAt, updatedAt 으로 Date형 객체 입력
+);
+
+// commentsSchema.virtual('commentId').get(function () {
+//     return this._id.toHexString();
+// });
+// commentsSchema.set('toJSON', {
+//     virtuals: true,
+// });
+
+const Comments = mongoose.model('Comments', commentsSchema);
+module.exports = Comments;
---
+++ routes/comment.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { json } = require("express/lib/response");
+const Comments = require('../models/comment');
+const router = express.Router();
+
+
+router.get("/", (req, res) =>{
+    res.send("this is root page");
+});
+
+// /api/list --> Json 형식으로 전송받음 -->list.html에서 ajax로 가져감.
+router.get("/main", async (req, res) => {
+    console.log("/api/main 연결")
+    try {
+        const comments = await Comments.find();
+        res.json({ result : comments });
+    } catch (err) {
+    console.error(err);
+    next(err);
+    }
+});
+
+// 댓글 작성
+router.post('/comments', async (req, res) => {
+    console.log('/api/comments 연결')
+    const { post_id, comment_id, user_name, comment, createdAt } = req.body;
+    console.log('0--->', { post_id, comment_id, user_name, comment, createdAt })
+
+    const sendComments = await Comments.create({ post_id, comment_id, user_name, comment, createdAt })
+
+    res.json({result : sendComments}); 
+    console.log(sendComments);
+
+});
+
+// 댓글 삭제
+router.delete("/delete/:comment_id", async (req, res,) => {
+    // html ajax --> 내용을 request 함. 
+    const { comment_id } = req.body;
+    const deleteComments = await Comments.deleteOne({comment_id:comment_id});
+  
+    res.json({result : deleteComments}); 
+
+  });
+  
+module.exports = router;
