63ad2d89c3366a165da66311cb5133976de359bf
jeongmin <101089290+jeongminGit@users.noreply.github.com>
2022-04-14 16:38:38 +0900

    bucket_name 수정

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -28,7 +28,7 @@ const fileFilter = (req, file, cb) => {
 
 const storage = multerS3({
     s3: S3,
-    bucket: "miniproject-spring-bucket",
+    bucket: process.env.BUCKET_NAME,
     key(req, file, cb) {
         cb(
             null,
